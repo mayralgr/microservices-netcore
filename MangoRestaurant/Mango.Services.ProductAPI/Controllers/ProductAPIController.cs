eb27af1bf77b3b8c956183173a9e7b8c945494a7
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2022-03-14 20:40:09 -0600

    Add product CRUD

--- MangoRestaurant/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -83,6 +83,7 @@ namespace Mango.Services.ProductAPI.Controllers
         }
 
         [HttpDelete]
+        [Route("{id}")]
         public async Task<ResponseDto> Delete(int id)
         {
             try

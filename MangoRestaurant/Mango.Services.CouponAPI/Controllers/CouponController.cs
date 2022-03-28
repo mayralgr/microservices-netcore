b4bda6341a8625e3893fd5ebb0c66ff5e5affe93
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2022-03-27 22:19:11 -0600

    Add couponApi and configure it

---
+++ MangoRestaurant/Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,34 @@
+﻿using Mango.Services.CouponAPI.Models.Dtos;
+using Mango.Services.CouponAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [ApiController]
+    [Route("api/coupon")]
+    public class CouponController : ControllerBase
+    {
+        private readonly ICouponRepository _couponRepository;
+        protected ResponseDto _response;
+
+        public CouponController(ICouponRepository couponRepository)
+        {
+            _couponRepository = couponRepository;
+            this._response = new ResponseDto();
+        }
+        [HttpGet("{code}")]
+        public  async Task<object> GetDiscountForCode(string code)
+        {
+            try
+            {
+                CouponDto couponDto = await _couponRepository.GetCouponByCode(code);
+                _response.Result = couponDto;
+            }catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages =  new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}
---
+++ MangoRestaurant/Mango.Services.CouponAPI/Models/Dtos/CouponDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.CouponAPI.Models.Dtos
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+    }
+}

e45a2a8a88e6cf05fcf75719a5d39a5f13da1bb4
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2022-04-15 18:39:14 -0500

    Add rabbitMQ settings and initial changes

---
+++ MangoRestaurant/Mango.Services.ShoppingCartAPI/RabbitMQSender/RabbitMQCartMessageSender.cs
@@ -0,0 +1,25 @@
+﻿using Mango.MessageBus;
+
+namespace Mango.Services.ShoppingCartAPI.RabbitMQSender
+{
+    public class RabbitMQCartMessageSender : IRabbitMQCartMessageSender
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _hostname;
+
+        private readonly string _username;
+
+        private readonly string _password;
+        public RabbitMQCartMessageSender(IConfiguration config)
+        {
+            _configuration = config;
+            _hostname = _configuration.GetSection("RabbitMQ").GetValue<string>("hostname");
+            _username = _configuration.GetSection("RabbitMQ").GetValue<string>("userName");
+            _password = _configuration.GetSection("RabbitMQ").GetValue<string>("password");
+        }
+        public void SendMessage(BaseMessage baseMessage, string queueName)
+        {
+
+        }
+    }
+}
